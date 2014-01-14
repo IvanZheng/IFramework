cb670f04e5701b5623ce43d120cc160bfbd2ca40
Ivan <yvonne_7_7@hotmail.com>
2014-01-14 11:23:06 +0800

    add lost file

---
+++ Src/iFramework/Message/Impl/MessageHandledNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IFramework.Message.Impl
+{
+    public class MessageHandledNotification : IMessageHandledNotification
+    {
+        public string MessageID { get; set; }
+
+        public MessageHandledNotification(string messageID)
+        {
+            MessageID = messageID;
+        }
+    }
+}
