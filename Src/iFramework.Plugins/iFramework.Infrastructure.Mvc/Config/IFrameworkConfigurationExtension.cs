fa0ec9b13e3bbfddf87d1ad810006e9faeaefea0
Ivan <yvonne_7_7@hotmail.com>
2014-02-08 10:47:04 +0800

    remove ioccontrollerfactory, use UnityDependencyResolver instead

--- Src/iFramework.Plugins/iFramework.Infrastructure.Mvc/Config/IFrameworkConfigurationExtension.cs
+++ Src/iFramework.Plugins/iFramework.Infrastructure.Mvc/Config/IFrameworkConfigurationExtension.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
-using IFramework.Infrastructure.ControllerFactories;
 using IFramework.Infrastructure;
 using Microsoft.Practices.Unity;
 using System.Reflection;
