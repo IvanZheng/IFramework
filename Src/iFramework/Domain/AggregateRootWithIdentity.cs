ae19431f9839316f5e55c01ec05c3b48ffab19a1
ivan <yvonne_7_7@hotmail.com>
2014-04-27 00:23:04 +0800

    Add AggregateRoot with identity

---
+++ Src/iFramework/Domain/AggregateRootWithIdentity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IFramework.Infrastructure;
+using Microsoft.Practices.Unity;
+using System.Reflection;
+using IFramework.Event;
+using IFramework.Event.Impl;
+
+namespace IFramework.Domain
+{
+    public abstract class Identity
+    {
+        public abstract new string ToString();
+    }
+
+    public abstract class AggregateRoot<TIdentity> : AggregateRoot
+        where TIdentity : Identity, new()
+    {
+        public TIdentity ID { get; protected set; }
+        public string Identity
+        {
+            get
+            {
+                return ID.ToString();
+            }
+            private set
+            {
+                ID = Activator.CreateInstance(typeof(TIdentity), value) as TIdentity;
+            }
+        }
+    }
+}
