acc4a65e01f767322e507f15aa5a92089dbb5175
IvanZheng <yvonne_7_7@hotmail.com>
2015-01-05 14:01:34 +0800

    add mock event subscriber

---
+++ Src/iFramework/Event/Impl/MockEventSubscriber.cs
@@ -0,0 +1,31 @@
+﻿using IFramework.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IFramework.Event.Impl
+{
+    public class MockEventSubscriber : IMessageConsumer
+    {
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public string GetStatus()
+        {
+            return string.Empty;
+        }
+
+        public decimal MessageCount
+        {
+            get { return 0; }
+        }
+    }
+}
