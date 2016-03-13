568b2b9801f83c327c266ec761244cb8cd0b53a5
ivanzheng <yvonne_7_7@hotmail.com>
2016-03-13 20:16:24 +0800

    add missing file

---
+++ Src/Sample/Sample.CommandHandlerTests/CommandHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using IFramework.Command;
+using IFramework.Infrastructure;
+using IFramework.Infrastructure.Unity.LifetimeManagers;
+using IFramework.Message;
+
+namespace Sample.CommandHandlerTests
+{
+    public class CommandHandlerTest<TCommandHanlder>
+    {
+        public object ExecuteCommand(ICommand command)
+        {
+            IMessageContext commandContext = new EmptyMessageContext(command);
+            PerMessageContextLifetimeManager.CurrentMessageContext = commandContext;
+            var commandHandler = IoCFactory.Resolve<TCommandHanlder>();
+            ((dynamic)commandHandler).Handle((dynamic)command);
+            var result = commandContext.Reply;
+            PerMessageContextLifetimeManager.CurrentMessageContext = null;
+            return result;
+        }
+    }
+}
