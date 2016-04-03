fc93efe73040eb55c6f296d2a5456263a19f70c5
ivanzheng <yvonne_7_7@hotmail.com>
2016-04-03 20:37:16 +0800

    add mysql support 1. add mysql conditional compilation symbol in IFramework project. 2. rebuild Iframework.dll for mysql support 3. reference  MySql.Data.Entity in nuget in the host project

--- Src/iFramework/Domain/TimestampedAggregateRoot.cs
+++ Src/iFramework/Domain/TimestampedAggregateRoot.cs
@@ -10,10 +10,17 @@ namespace IFramework.Domain
     public class TimestampedAggregateRoot : AggregateRoot
     {
         [Timestamp]
+#if mysql
+        public DateTime Version
+#else
         public byte[] Version
+#endif
         {
             get;
             private set;
         }
+
+
     }
+
 }
