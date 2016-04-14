579fdda1ec3aeee75953ee7a3eaf05ac5edea1ab
ivanzheng <yvonne_7_7@hotmail.com>
2016-04-14 21:51:03 +0800

    add applicationusermanager in CustomSecurityTokenService

--- Src/iFramework.Plugins/IFramework.Sample.AuthWeb/Controllers/AccountController.cs
+++ Src/iFramework.Plugins/IFramework.Sample.AuthWeb/Controllers/AccountController.cs
@@ -3,12 +3,12 @@ using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using IFramework.Sample.AuthWeb.Models;
+using System.Web;
 
 namespace IFramework.Sample.AuthWeb.Controllers
 {
--- Src/iFramework.Plugins/IFramework.Sample.AuthWeb/IdentityProvider/CustomSecurityTokenService.cs
+++ Src/iFramework.Plugins/IFramework.Sample.AuthWeb/IdentityProvider/CustomSecurityTokenService.cs
@@ -5,19 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using IFramework.SingleSignOn.IdentityProvider;
 using System.IdentityModel.Configuration;
+using IFramework.Infrastructure;
+using Microsoft.AspNet.Identity.EntityFramework;
+using IFramework.Sample.AuthWeb.Models;
 
 namespace IFramework.Sample.AuthWeb.IdentityProvider
 {
+
     public class CustomSecurityTokenService : IFramework.SingleSignOn.IdentityProvider.CustomSecurityTokenService
     {
+
+        ApplicationUserManager _applicationUserManager;
         public CustomSecurityTokenService(SecurityTokenServiceConfiguration config)
            : base(config)
         {
+            _applicationUserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(new ApplicationDbContext()));
         }
 
         protected override CustomIdentityObject GetCustomIdentity(string identity)
         {
-            return new CustomIdentityObject { Identity = identity, Name = DateTime.Now.ToString() };
+            var user = _applicationUserManager.FindByNameAsync(identity).Result;
+            return new CustomIdentityObject { Identity = user.Id, Name = user.UserName };
         }
     }
 }
