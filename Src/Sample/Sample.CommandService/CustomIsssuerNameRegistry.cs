7cec2d07366eb5bfec6fe23cb09f98069a1f9d6e
ivanzheng <yvonne_7_7@hotmail.com>
2016-04-14 21:11:47 +0800

    correct thumbprint

---
+++ Src/Sample/Sample.CommandService/CustomIsssuerNameRegistry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Sample.CommandService
+{
+    public class CustomIsssuerNameRegistry : ConfigurationBasedIssuerNameRegistry
+    {
+        public override string GetIssuerName(System.IdentityModel.Tokens.SecurityToken securityToken)
+        {
+            if (securityToken == null)
+            {
+                throw new Exception("securityToken");
+            }
+            X509SecurityToken token = securityToken as X509SecurityToken;
+            if (token != null)
+            {
+                string thumbprint = token.Certificate.Thumbprint;
+                if (this.ConfiguredTrustedIssuers.ContainsKey(thumbprint)) //Breakpoint here
+                {
+                    return this.ConfiguredTrustedIssuers[thumbprint];
+                }
+            }
+            return null;
+            //var ret = base.GetIssuerName(securityToken);
+            //return ret;
+        }
+
+        public override string GetIssuerName(System.IdentityModel.Tokens.SecurityToken securityToken, string requestedIssuerName)
+        {
+            var ret = base.GetIssuerName(securityToken, requestedIssuerName);
+
+            return ret;
+        }
+    }
+}
