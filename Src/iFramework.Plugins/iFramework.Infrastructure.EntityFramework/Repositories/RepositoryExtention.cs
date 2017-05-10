80d34850e7f2c4add90643b85c292c159785f876
ivanzheng <yvonne_7_7@hotmail.com>
2017-05-10 17:15:33 +0800

    add FirstOrDefaultAsync

--- Src/iFramework.Plugins/iFramework.Infrastructure.EntityFramework/Repositories/RepositoryExtention.cs
+++ Src/iFramework.Plugins/iFramework.Infrastructure.EntityFramework/Repositories/RepositoryExtention.cs
@@ -25,9 +25,36 @@ namespace IFramework.EntityFramework.Repositories
             (repository as DomainRepository).GetRepository<TEntity>().AddWithSaveChanges(entity);
         }
 
+        public static IQueryable Include(this IQueryable source, string path)
+        {
+            return QueryableExtensions.Include(source, path);
+        }
+
+        public static IQueryable<T> Include<T>(this IQueryable<T> source, string path)
+        {
+            return QueryableExtensions.Include(source, path);
+        }
+
+        public static IQueryable<T> Include<T, TProperty>(this IQueryable<T> source, Expression<Func<T, TProperty>> path)
+        {
+            return QueryableExtensions.Include(source, path);
+        }
+
         public static Task<List<TEntity>> ToListAsync<TEntity>(this IQueryable<TEntity> query) where TEntity : class
         {
             return QueryableExtensions.ToListAsync(query);
+        }
+
+        public static Task<TEntity> FirstOrDefaultAsync<TEntity>(this IQueryable<TEntity> query, Expression<Func<TEntity, bool>> predicate = null) where TEntity : class
+        {
+            if (predicate == null)
+            {
+                return QueryableExtensions.FirstOrDefaultAsync(query);
+            }
+            else
+            {
+                return QueryableExtensions.FirstOrDefaultAsync(query, predicate);
+            }
         }
     }
 }
