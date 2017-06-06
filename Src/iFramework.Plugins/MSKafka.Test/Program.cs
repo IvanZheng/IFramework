b22b6d91015c0516e8f38bc7df8607e6368ce998
IvanZheng <yvonne_7_7@hotmail.com>
2017-06-07 00:18:18 +0800

    clean up code

--- Src/iFramework.Plugins/MSKafka.Test/Program.cs
+++ Src/iFramework.Plugins/MSKafka.Test/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using IFramework.Command;
 using IFramework.Config;
 using IFramework.Infrastructure;
 using IFramework.IoC;
 using IFramework.MessageQueue.ConfluentKafka;
 using IFramework.MessageQueue.ConfluentKafka.MessageFormat;
-using Kafka.Client.Consumers;
 using Sample.Command;
 using Sample.DTO;
+
 //using IFramework.MessageQueue.MSKafka;
 
 namespace KafkaClient.Test
@@ -39,7 +37,6 @@ namespace KafkaClient.Test
                 Console.WriteLine(
                                   $"consumer:{kafkaConsumer.ConsumerId} {DateTime.Now.ToString("HH:mm:ss.fff")} consume message: {message} cost: {(DateTime.Now - sendTime).TotalMilliseconds} partition:{kafkaMessage.Partition} offset:{kafkaMessage.Offset}");
                 kafkaConsumer.CommitOffsetAsync(kafkaMessage.Partition, kafkaMessage.Offset);
-
             };
             var consumer = new KafkaConsumer(zkConnectionString, commandQueue,
                                              $"{Environment.MachineName}.{commandQueue}", consumerId, onMessageReceived);
@@ -54,7 +51,7 @@ namespace KafkaClient.Test
                 consumers.Add(CreateConsumer(commandQueue, i.ToString()));
             }
             var queueClient = new KafkaProducer(commandQueue, zkConnectionString);
-            int times = 0;
+            var times = 0;
             while (true)
             {
                 //if (times++ % 10 == 0)
@@ -62,8 +59,8 @@ namespace KafkaClient.Test
                 //    Task.Delay(10).Wait();
                 //}
                 //var message = string.Empty;
-                var message = Console.ReadLine();
-                if (message.Equals("q"))
+                var key = Console.ReadLine();
+                if (key.Equals("q"))
                 {
                     consumers.ForEach(consumer => consumer.Stop());
                     queueClient.Stop();
@@ -71,32 +68,21 @@ namespace KafkaClient.Test
                     break;
                 }
                 //Console.WriteLine($"receive input {message}");
-                message = $"{message} @{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffffff}";
+                var message = $"{key} @{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffffff}";
                 var kafkaMessage = new KafkaMessage(Encoding.UTF8.GetBytes(message));
                 //var data = new ProducerData<string, Message>(commandQueue, message, kafkaMessage);
 
-                while (true)
-                {
-                    try
-                    {
-                        var start = DateTime.Now;
-                        queueClient.SendAsync(message, kafkaMessage)
-                                   .ContinueWith(t =>
-                                   {
-                                       var result = t.Result;
-                                       Console.WriteLine($"send message: {message} partition:{result.Partition} offset:{result.Offset} cost: {(DateTime.Now - start).TotalMilliseconds}");
-                                   });
 
-                        break;
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("send message failed {0}", ex.Message);
-                        Thread.Sleep(2000);
-                    }
-                }
+                var start = DateTime.Now;
+                queueClient.SendAsync(key, kafkaMessage)
+                           .ContinueWith(t =>
+                           {
+                               var result = t.Result;
+                               Console.WriteLine($"send message: {message} partition:{result.Partition} offset:{result.Offset} cost: {(DateTime.Now - start).TotalMilliseconds}");
+                           });
             }
         }
+
 
         private static void ServiceTest()
         {
@@ -114,9 +100,9 @@ namespace KafkaClient.Test
 
 
             var products = _commandBus.SendAsync(new GetProducts
-            {
-                ProductIds = new List<Guid> { reduceProduct.ProductId }
-            }, true)
+                                      {
+                                          ProductIds = new List<Guid> {reduceProduct.ProductId}
+                                      }, true)
                                       .Result.ReadAsAsync<List<Project>>()
                                       .Result;
 
