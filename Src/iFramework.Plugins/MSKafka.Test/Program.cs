f7f6012be22b39d20cc5eba66eb4badddd417c19
ivanzheng <yvonne_7_7@hotmail.com>
2017-07-19 11:18:24 +0800

    clean up code

--- Src/iFramework.Plugins/MSKafka.Test/Program.cs
+++ Src/iFramework.Plugins/MSKafka.Test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using Confluent.Kafka;
 using IFramework.Command;
 using IFramework.Config;
 using IFramework.Infrastructure;
@@ -18,12 +19,13 @@ namespace KafkaClient.Test
     internal class Program
     {
         private static readonly string commandQueue = "confluentconsole.groupcommandqueue";
-        private static readonly string zkConnectionString = "localhost:9092";
+        private static readonly string brokerList = "localhost:9092";
 
         private static void Main(string[] args)
         {
             Configuration.Instance
                          .UseUnityContainer()
+                         .UseJsonNet()
                          .UseLog4Net("log4net.config");
             GroupConsuemrTest();
             IoCFactory.Instance.CurrentContainer.Dispose();
@@ -31,16 +33,16 @@ namespace KafkaClient.Test
 
         public static KafkaConsumer CreateConsumer(string commandQueue, string consumerId)
         {
-            OnKafkaMessageReceived onMessageReceived = (kafkaConsumer, kafkaMessage) =>
+            void OnMessageReceived(KafkaConsumer kafkaConsumer, Message<string, KafkaMessage> kafkaMessage)
             {
                 var message = kafkaMessage.Value.Payload;
                 var sendTime = DateTime.Parse(message.Split('@')[1]);
-                Console.WriteLine(
-                                  $"consumer:{kafkaConsumer.ConsumerId} {DateTime.Now.ToString("HH:mm:ss.fff")} consume message: {message} cost: {(DateTime.Now - sendTime).TotalMilliseconds} partition:{kafkaMessage.Partition} offset:{kafkaMessage.Offset}");
+                Console.WriteLine($"consumer:{kafkaConsumer.ConsumerId} {DateTime.Now:HH:mm:ss.fff} consume message: {message} cost: {(DateTime.Now - sendTime).TotalMilliseconds} partition:{kafkaMessage.Partition} offset:{kafkaMessage.Offset}");
                 kafkaConsumer.CommitOffsetAsync(kafkaMessage.Partition, kafkaMessage.Offset);
-            };
-            var consumer = new KafkaConsumer(zkConnectionString, commandQueue,
-                                             $"{Environment.MachineName}.{commandQueue}", consumerId, onMessageReceived);
+            }
+
+            var consumer = new KafkaConsumer(brokerList, commandQueue,
+                                             $"{Environment.MachineName}.{commandQueue}", consumerId, OnMessageReceived);
             return consumer;
         }
 
@@ -51,28 +53,20 @@ namespace KafkaClient.Test
             {
                 consumers.Add(CreateConsumer(commandQueue, i.ToString()));
             }
-            var queueClient = new KafkaProducer(commandQueue, zkConnectionString);
-            var times = 0;
+            var queueClient = new KafkaProducer(commandQueue, brokerList);
             while (true)
             {
-                //if (times++ % 10 == 0)
-                //{
-                //    Task.Delay(10).Wait();
-                //}
-                //var message = string.Empty;
+                
                 var key = Console.ReadLine();
                 if (key.Equals("q"))
                 {
                     consumers.ForEach(consumer => consumer.Stop());
                     queueClient.Stop();
-                    //ZookeeperConsumerConnector.zkClientStatic?.Dispose();
                     break;
                 }
-                //Console.WriteLine($"receive input {message}");
+
                 var message = $"{key} @{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffffff}";
                 var kafkaMessage = new KafkaMessage(message);
-                //var data = new ProducerData<string, Message>(commandQueue, message, kafkaMessage);
-
 
                 var start = DateTime.Now;
                 queueClient.SendAsync(key, kafkaMessage, CancellationToken.None)
