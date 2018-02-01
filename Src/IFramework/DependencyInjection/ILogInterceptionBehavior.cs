728c92bda75fd329bca60b94280fad69e2e26ebc
Ivan Zheng <yvonne_7_7@hotmail.com>
2018-02-01 17:29:32 +0800

    clean up code

---
+++ Src/IFramework/DependencyInjection/ILogInterceptionBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace IFramework.DependencyInjection
+{
+    public interface ILogInterceptionBehavior
+    {
+        void BeforeInvoke(ILogger logger, MethodInfo method, object target, object[] arguments);
+        void AfterInvoke(ILogger logger, MethodInfo method, object target, DateTime start, object result, Exception exception);
+        void HandleException(ILogger logger, MethodInfo method, object target, Exception exception);
+    }
+}
