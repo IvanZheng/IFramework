2e79c0bd03dae2a5dc27d92fe74e0f3a0505fbea
Ivan <yvonne_7_7@hotmail.com>
2018-02-08 21:41:37 +0800

    add command controller

---
+++ Src/Sample/Sample.CommandServiceCore/Controllers/ApiControllerBase.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using IFramework.Exceptions;
+using IFramework.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Net.Http.Headers;
+
+namespace Sample.CommandServiceCore.Controllers
+{
+    public class ApiControllerBase : Controller
+    {
+        public ApiControllerBase(IExceptionManager exceptionManager)
+        {
+            ExceptionManager = exceptionManager;
+        }
+
+        protected IExceptionManager ExceptionManager { get; }
+
+        protected virtual string GetModelErrorMessage(ModelStateDictionary modelState)
+        {
+            return string.Join(";", modelState.Where(m => (m.Value?.Errors?.Count ?? 0) > 0)
+                                              .Select(m => $"{m.Key}:{string.Join(",", m.Value.Errors.Select(e => e.ErrorMessage + e.Exception?.Message))}"));
+        }
+
+        #region process wrapping
+
+        protected virtual ApiResult<T> Process<T>(Func<T> func,
+                                                  bool needRetry = true,
+                                                  Func<Exception, string> getExceptionMessage = null,
+                                                  Func<ModelStateDictionary, string> getModelErrorMessage = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var apiResult = ExceptionManager.Process(func, needRetry, getExceptionMessage: getExceptionMessage);
+                return apiResult;
+            }
+            getModelErrorMessage = getModelErrorMessage ?? GetModelErrorMessage;
+            return
+                new ApiResult<T>
+                    (
+                     ErrorCode.InvalidParameters,
+                     getModelErrorMessage(ModelState)
+                    );
+        }
+
+        protected virtual ApiResult Process(Action action,
+                                            bool needRetry = true,
+                                            Func<Exception, string> getExceptionMessage = null,
+                                            Func<ModelStateDictionary, string> getModelErrorMessage = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var apiResult = ExceptionManager.Process(action, needRetry, getExceptionMessage: getExceptionMessage);
+                return apiResult;
+            }
+            getModelErrorMessage = getModelErrorMessage ?? GetModelErrorMessage;
+            return
+                new ApiResult
+                    (
+                     ErrorCode.InvalidParameters,
+                     getModelErrorMessage(ModelState)
+                    );
+        }
+        
+        protected virtual async Task<ApiResult> ProcessAsync(Func<Task> func,
+                                                             bool continueOnCapturedContext = false,
+                                                             bool needRetry = true,
+                                                             Func<Exception, string> getExceptionMessage = null,
+                                                             Func<ModelStateDictionary, string> getModelErrorMessage = null)
+        {
+            if (ModelState.IsValid)
+            {
+                return await ExceptionManager.ProcessAsync(func,
+                                                           continueOnCapturedContext,
+                                                           needRetry,
+                                                           getExceptionMessage: getExceptionMessage)
+                                             .ConfigureAwait(continueOnCapturedContext);
+            }
+            getModelErrorMessage = getModelErrorMessage ?? GetModelErrorMessage;
+            return
+                new ApiResult
+                    (
+                     ErrorCode.InvalidParameters,
+                     getModelErrorMessage(ModelState)
+                    );
+        }
+
+        protected virtual async Task<ApiResult<T>> ProcessAsync<T>(Func<Task<T>> func,
+                                                                   bool continueOnCapturedContext = false,
+                                                                   bool needRetry = true,
+                                                                   Func<Exception, string> getExceptionMessage = null,
+                                                                   Func<ModelStateDictionary, string> getModelErrorMessage = null)
+        {
+            if (ModelState.IsValid)
+            {
+                return await ExceptionManager.ProcessAsync(func, continueOnCapturedContext, needRetry,
+                                                           getExceptionMessage: getExceptionMessage)
+                                             .ConfigureAwait(continueOnCapturedContext);
+            }
+            getModelErrorMessage = getModelErrorMessage ?? GetModelErrorMessage;
+            return
+                new ApiResult<T>
+                    (
+                     ErrorCode.InvalidParameters,
+                     getModelErrorMessage(ModelState)
+                    );
+        }
+        #endregion
+    }
+}
---
+++ Src/Sample/Sample.CommandServiceCore/Controllers/CommandController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IFramework.Command;
+using IFramework.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.CommandServiceCore.Controllers
+{
+    public class CommandController : ApiControllerBase
+    {
+        private readonly ICommandBus _commandBus;
+
+        public CommandController(ICommandBus commandBus, IExceptionManager exceptionManager)
+            : base(exceptionManager)
+        {
+            _commandBus = commandBus;
+        }
+
+        public Task<ApiResult<object>> Post([FromBody] ICommand command)
+        {
+            return ProcessAsync(() => _commandBus.ExecuteAsync(command));
+        }
+    }
+}
