5643a28c1afdd37f62cb3f87e43d95ddaf7aa706
Ivan Zheng <yvonne_7_7@hotmail.com>
2018-02-12 09:14:23 +0800

    use ajax application/x-www-form-urlencoded to post command

--- Src/Sample/Sample.Command/Community/Register.cs
+++ Src/Sample/Sample.Command/Community/Register.cs
@@ -5,6 +5,12 @@
         public string Email { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string[] Tags { get; set; }
+        public Tag[] Tags { get; set; }
+    }
+
+    public class Tag
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
