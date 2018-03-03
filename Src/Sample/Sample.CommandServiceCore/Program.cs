d4187cc136e23c07058e4ea79159df74c108bdd0
Ivan <yvonne_7_7@hotmail.com>
2018-03-04 00:32:20 +0800

    clean up code

--- Src/Sample/Sample.CommandServiceCore/Program.cs
+++ Src/Sample/Sample.CommandServiceCore/Program.cs
@@ -13,10 +13,21 @@ namespace Sample.CommandServiceCore
 
         public static IWebHost BuildWebHost(string[] args)
         {
-            return WebHost.CreateDefaultBuilder(args)
-                          .UseUrls(new ConfigurationBuilder().AddCommandLine(args).Build()["urls"])
-                          .UseStartup<Startup>()
-                          .Build();
+            var configRoot = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var urls = configRoot.GetValue("urls", string.Empty);
+            var environment = configRoot.GetValue("environment", string.Empty);
+
+            var build = WebHost.CreateDefaultBuilder(args);
+            if (!string.IsNullOrWhiteSpace(urls))
+            {
+                build.UseUrls(urls);
+            }
+            if (!string.IsNullOrWhiteSpace(environment))
+            {
+                build.UseEnvironment(environment);
+            }
+            return build.UseStartup<Startup>()
+                        .Build();
         }
     }
 }
