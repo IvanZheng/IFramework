4ee0658fdc593d32838db8a209c0c8d6fca632ae
IvanZheng <yvonne_7_7@hotmail.com>
2018-08-18 15:19:31 +0800

    add rabbitmq proejct

---
+++ Src/iFramework.Plugins/IFramework.MessageQueue.RabbitMQ/RabbitMQClientProvider.cs
@@ -0,0 +1,40 @@
+﻿using IFramework.MessageQueue.Client.Abstracts;
+using System;
+using IFramework.Message;
+using IFramework.Message.Impl;
+
+namespace IFramework.MessageQueue.RabbitMQ
+{
+    public class RabbitMQClientProvider: IMessageQueueClientProvider
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageContext WrapMessage(object message, string correlationId = null, string topic = null, string key = null, string replyEndPoint = null, string messageId = null, SagaInfo sagaInfo = null, string producer = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageConsumer CreateQueueConsumer(string commandQueueName, OnMessagesReceived onMessagesReceived, string consumerId, ConsumerConfig consumerConfig, bool start = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageConsumer CreateTopicSubscription(string topic, string subscriptionName, OnMessagesReceived onMessagesReceived, string consumerId, ConsumerConfig consumerConfig, bool start = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageProducer CreateTopicProducer(string topic, ProducerConfig config = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageProducer CreateQueueProducer(string queue, ProducerConfig config = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
