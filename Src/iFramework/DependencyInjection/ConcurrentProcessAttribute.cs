a5b69da9215bb904b79bcffae12f748949546e5f
Ivan Zheng <yvonne_7_7@hotmail.com>
2018-08-24 08:52:06 +0800

    add retrytime for ConcurrentProcessAttribute

--- Src/iFramework/DependencyInjection/ConcurrentProcessAttribute.cs
+++ Src/iFramework/DependencyInjection/ConcurrentProcessAttribute.cs
@@ -7,6 +7,11 @@ namespace IFramework.DependencyInjection
 {
     public class ConcurrentProcessAttribute : InterceptorAttribute
     {
+        public ConcurrentProcessAttribute(int retryTimes = 50)
+        {
+            RetryTimes = retryTimes;
+        }
+        public int RetryTimes { get; set; }
         public override Task<object> ProcessAsync(Func<Task<object>> funcAsync,
                                                   IObjectProvider objectProvider,
                                                   Type targetType,
@@ -15,7 +20,7 @@ namespace IFramework.DependencyInjection
                                                   MethodInfo methodInvocationTarget)
         {
             var concurrencyProcessor = objectProvider.GetService<IConcurrencyProcessor>();
-            return concurrencyProcessor.ProcessAsync(funcAsync);
+            return concurrencyProcessor.ProcessAsync(funcAsync, RetryTimes);
         }
 
         public override object Process(Func<object> func,
@@ -26,7 +31,7 @@ namespace IFramework.DependencyInjection
                                        MethodInfo methodInvocationTarget)
         {
             var concurrencyProcessor = objectProvider.GetService<IConcurrencyProcessor>();
-            return concurrencyProcessor.Process(func);
+            return concurrencyProcessor.Process(func, RetryTimes);
         }
     }
 }
