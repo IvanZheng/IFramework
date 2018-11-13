a06e4b65698d513295a574d2cd5236451c707c0c
Ivan Zheng <yvonne_7_7@hotmail.com>
2018-11-13 10:36:15 +0800

    clean up code

--- Src/IFramework.Test/EntityFramework/EntityFrameworkTests.cs
+++ Src/IFramework.Test/EntityFramework/EntityFrameworkTests.cs
@@ -7,8 +7,6 @@ using System.Transactions;
 using IFramework.Config;
 using IFramework.DependencyInjection;
 using IFramework.DependencyInjection.Autofac;
-using IFramework.DependencyInjection.Microsoft;
-using IFramework.DependencyInjection.Unity;
 using IFramework.Domain;
 using IFramework.EntityFrameworkCore;
 using IFramework.Infrastructure;
@@ -25,12 +23,12 @@ using ObjectProvider = IFramework.DependencyInjection.Unity.ObjectProvider;
 
 namespace IFramework.Test.EntityFramework
 {
-
     public class DemoDbContextFactory : IDesignTimeDbContextFactory<DemoDbContext>
     {
         public static string MySqlConnectionStringName = "DemoDbContext.MySql";
         public static string ConnectionStringName = "DemoDbContext";
         public static string MongoDbConnectionStringName = "DemoDbContext.MongoDb";
+
         public DemoDbContext CreateDbContext(string[] args)
         {
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
@@ -78,15 +76,131 @@ namespace IFramework.Test.EntityFramework
 
         public class DbTest : IDisposable
         {
-            public int Count { get; }
-
             public DbTest(int count)
             {
                 Count = count;
             }
+
+            public int Count { get; }
+
             public void Dispose()
             {
                 Console.Write("dispose");
+            }
+        }
+
+        [Fact]
+        public async Task AddPresonTest()
+        {
+            try
+            {
+                using (var serviceScope = ObjectProviderFactory.CreateScope())
+                {
+                    var dbContext = serviceScope.GetService<DemoDbContext>();
+                    var person = new Person("ivan");
+                    dbContext.Persons.Add(person);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [Fact]
+        public async Task AddUserTest()
+        {
+            using (var serviceScope = ObjectProviderFactory.CreateScope())
+            using (var scope = new TransactionScope(TransactionScopeOption.Required,
+                                                    new TransactionOptions {IsolationLevel = IsolationLevel.ReadCommitted},
+                                                    TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var serviceProvider = serviceScope.GetService<IServiceProvider>();
+                if (serviceProvider == null)
+                {
+                    Assert.NotNull(serviceProvider);
+                }
+
+                try
+                {
+                    var dbContext = serviceScope.GetService<DemoDbContext>();
+                    if (dbContext == null)
+                    {
+                        var logger = ObjectProviderFactory.GetService<ILoggerFactory>().CreateLogger(GetType());
+                        logger.LogError((serviceScope as ObjectProvider)?.UnityContainer.Registrations.ToJson());
+                        Assert.NotNull(dbContext);
+                    }
+
+                    var user = new User("ivan", "male");
+                    user.AddCard("ICBC");
+                    user.AddCard("CCB");
+                    user.AddCard("ABC");
+
+                    dbContext.Users.Add(user);
+                    await dbContext.SaveChangesAsync();
+                    scope.Complete();
+                    var client = dbContext.GetMongoDbClient();
+                    var database = dbContext.GetMongoDbDatabase();
+                    var conn = dbContext.GetMongoDbConnection();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        [Fact]
+        public async Task ConcurrentTest()
+        {
+            var logger = ObjectProviderFactory.GetService<ILoggerFactory>().CreateLogger(GetType());
+
+            try
+            {
+                //await AddUserTest();
+                var tasks = new List<Task>();
+                for (int i = 0; i < 5; i++)
+                {
+                    tasks.Add(GetUsersTest());
+                }
+
+                await Task.WhenAll(tasks);
+
+                logger.LogDebug($"incremented : {DemoDbContext.Total}");
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"incremented : {DemoDbContext.Total}");
+                throw;
+            }
+        }
+
+
+        [Fact]
+        public async Task ConcurrentUdpateTest()
+        {
+            using (var serviceScope = ObjectProviderFactory.CreateScope())
+            {
+                var concurrencyProcessor = serviceScope.GetService<IConcurrencyProcessor>();
+                var dbContext = serviceScope.GetService<DemoDbContext>();
+                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required,
+                                                                   new TransactionOptions
+                                                                   {
+                                                                       IsolationLevel = IsolationLevel.ReadCommitted
+                                                                   },
+                                                                   TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    await concurrencyProcessor.ProcessAsync(async () =>
+                    {
+                        var account = await dbContext.Users.FirstOrDefaultAsync();
+                        account.ModifyName($"ivan{DateTime.Now}");
+                        await dbContext.SaveChangesAsync();
+                    });
+                    transactionScope.Complete();
+                }
             }
         }
 
@@ -118,129 +232,6 @@ namespace IFramework.Test.EntityFramework
         }
 
         [Fact]
-        public async Task AddPresonTest()
-        {
-            try
-            {
-                using (var serviceScope = ObjectProviderFactory.CreateScope())
-                {
-                    var dbContext = serviceScope.GetService<DemoDbContext>();
-                    var person = new Person("ivan");
-                    dbContext.Persons.Add(person);
-                    await dbContext.SaveChangesAsync();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-
-        }
-
-
-
-
-        [Fact]
-        public async Task ConcurrentUdpateTest()
-        {
-            using (var serviceScope = ObjectProviderFactory.CreateScope())
-            {
-                var concurrencyProcessor = serviceScope.GetService<IConcurrencyProcessor>();
-                var dbContext = serviceScope.GetService<DemoDbContext>();
-                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required,
-                                                                   new TransactionOptions
-                                                                   {
-                                                                       IsolationLevel = IsolationLevel.ReadCommitted
-                                                                   },
-                                                                   TransactionScopeAsyncFlowOption.Enabled))
-                {
-                    await concurrencyProcessor.ProcessAsync(async () =>
-                    {
-                        var account = await dbContext.Users.FirstOrDefaultAsync();
-                        account.ModifyName($"ivan{DateTime.Now}");
-                        await dbContext.SaveChangesAsync();
-                    });
-                    transactionScope.Complete();
-                }
-            }
-        }
-
-        [Fact]
-        public async Task ConcurrentTest()
-        {
-            var logger = ObjectProviderFactory.GetService<ILoggerFactory>().CreateLogger(GetType());
-
-            try
-            {
-                //await AddUserTest();
-                var tasks = new List<Task>();
-                for (int i = 0; i < 5; i++)
-                {
-                    tasks.Add(GetUsersTest());
-                }
-
-                await Task.WhenAll(tasks);
-
-                logger.LogDebug($"incremented : {DemoDbContext.Total }");
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, $"incremented : {DemoDbContext.Total }");
-                throw;
-            }
-        }
-
-        [Fact]
-        public async Task AddUserTest()
-        {
-
-            using (var serviceScope = ObjectProviderFactory.CreateScope())
-            using (var scope = new TransactionScope(TransactionScopeOption.Required,
-                                                    new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted },
-                                                    TransactionScopeAsyncFlowOption.Enabled))
-            {
-                var serviceProvider = serviceScope.GetService<IServiceProvider>();
-                if (serviceProvider == null)
-                {
-                    Assert.NotNull(serviceProvider);
-                }
-
-                try
-                {
-                    var dbContext = serviceScope.GetService<DemoDbContext>();
-                    if (dbContext == null)
-                    {
-                        var logger = ObjectProviderFactory.GetService<ILoggerFactory>().CreateLogger(GetType());
-                        logger.LogError((serviceScope as ObjectProvider)?.UnityContainer.Registrations.ToJson());
-                        Assert.NotNull(dbContext);
-                    }
-
-                    var user = new User("ivan", "male");
-                    user.AddCard("ICBC");
-                    user.AddCard("CCB");
-                    user.AddCard("ABC");
-
-                    dbContext.Users.Add(user);
-                    await dbContext.SaveChangesAsync();
-                    scope.Complete();
-                    var client = dbContext.GetMongoDbClient();
-                    var database = dbContext.GetMongoDbDatabase();
-                    var conn = dbContext.GetMongoDbConnection();
-
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    throw;
-                }
-
-
-
-            }
-        }
-
-        [Fact]
         public async Task GetUsersTest()
         {
             using (var scope = ObjectProviderFactory.CreateScope())
@@ -253,32 +244,31 @@ namespace IFramework.Test.EntityFramework
                     Assert.NotNull(serviceProvider);
                 }
 
-                var options = new DbContextOptionsBuilder<DemoDbContext>();
-                options.UseMongoDb(Configuration.Instance.GetConnectionString(DemoDbContextFactory.MongoDbConnectionStringName));
+                //var options = new DbContextOptionsBuilder<DemoDbContext>();
+                //options.UseMongoDb(Configuration.Instance.GetConnectionString(DemoDbContextFactory.MongoDbConnectionStringName));
 
-                using (var dbContext = new DemoDbContext(options.Options)) //scope.GetService<DemoDbContext>();
+                var dbContext = scope.GetService<DemoDbContext>();
+
+                try
                 {
-                    try
+                    var connection = dbContext.GetMongoDbDatabase();
+                    var users = await dbContext.Users
+                                               //.Include(u => u.Cards)
+                                               //.FindAll(u => !string.IsNullOrWhiteSpace(u.Name))
+                                               .Take(10)
+                                               .ToListAsync()
+                                               .ConfigureAwait(false);
+                    foreach (var u in users)
                     {
-                        var connection = dbContext.GetMongoDbDatabase();
-                        var users = await dbContext.Users
-                                                   //.Include(u => u.Cards)
-                                                   //.FindAll(u => !string.IsNullOrWhiteSpace(u.Name))
-                                                   .Take(10)
-                                                   .ToListAsync()
-                                                   .ConfigureAwait(false);
-                        foreach (var u in users)
-                        {
-                            await u.LoadCollectionAsync(u1 => u1.Cards);
-                            Assert.True(u.Cards.Count > 0);
-                            //Assert.Equal(u.GetDbContext<DemoDbContext>().GetHashCode(), dbContext.GetHashCode());
-                        }
+                        await u.LoadCollectionAsync(u1 => u1.Cards);
+                        Assert.True(u.Cards.Count > 0);
+                        //Assert.Equal(u.GetDbContext<DemoDbContext>().GetHashCode(), dbContext.GetHashCode());
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        throw;
-                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
                 }
             }
         }
