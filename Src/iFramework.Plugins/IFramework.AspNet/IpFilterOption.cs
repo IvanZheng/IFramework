fc50200a5da97f00ad661741868a5aacb930d065
Ivan Zheng <yvonne_7_7@hotmail.com>
2019-02-25 18:12:20 +0800

    Add IpFilterAttribute

---
+++ Src/iFramework.Plugins/IFramework.AspNet/IpFilterOption.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace IFramework.AspNet
+{
+    public class IpFilterOption
+    {
+        public bool Enabled { get; set; }
+        public IpFilterOption()
+        {
+            GlobalWhiteList = new List<string>();
+            EntryWhiteListDictionary = new Dictionary<string, List<string>>();
+        }
+
+        public List<string> GlobalWhiteList { get; set; }
+
+        public Dictionary<string, List<string>> EntryWhiteListDictionary { get; set; }
+    }
+}
