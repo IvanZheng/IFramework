e905b9a94741faf6d08e7664e25dbb574ad608f7
Ivan Zheng <yvonne_7_7@hotmail.com>
2019-03-04 10:18:16 +0800

    clean up code

--- Src/iFramework/Infrastructure/UniqueConstrainExceptionParser.cs
+++ Src/iFramework/Infrastructure/UniqueConstrainExceptionParser.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
-using System.Text;
 
 namespace IFramework.Infrastructure
 {
-    public class UniqueConstrainExceptionParser: IUniqueConstrainExceptionParser
+    public class UniqueConstrainExceptionParser : IUniqueConstrainExceptionParser
     {
         public bool IsUniqueConstrainException(Exception exception, string[] uniqueConstrainNames)
         {
@@ -14,10 +12,11 @@ namespace IFramework.Infrastructure
             if (uniqueConstrainNames?.Length > 0 && exception.GetBaseException() is DbException dbException)
             {
                 var number = dbException.GetPropertyValue<int>("Number");
-                needRetry = (dbException.Source.Contains("MySql") && number == 1062 ||
-                             dbException.Source.Contains("SqlClient") && (number == 2601 || number == 2627 || number == 547) ||
-                             dbException.Source.Contains("Npgsql") && number == 23505) &&
-                            uniqueConstrainNames.Any(dbException.Message.Contains);
+                needRetry = (
+                                number == 1062 && dbException.Source.Contains("MySql") ||
+                                (number == 2601 || number == 2627 || number == 547) && dbException.Source.Contains("SqlClient") ||
+                                number == 23505 && dbException.Source.Contains("Npgsql")
+                            ) && uniqueConstrainNames.Any(dbException.Message.Contains);
             }
 
             return needRetry;
