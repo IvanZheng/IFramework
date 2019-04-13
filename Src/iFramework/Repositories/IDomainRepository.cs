b0df4ffffc481a03091b456d6ead7de149b7cb3d
Ivan <yvonne_7_7@hotmail.com>
2019-04-13 13:06:10 +0800

    rename IDomainRepository.PageFind return tuple

--- Src/iFramework/Repositories/IDomainRepository.cs
+++ Src/iFramework/Repositories/IDomainRepository.cs
@@ -132,25 +132,25 @@ namespace IFramework.Repositories
         /// <param name="entity">The entity to be updated.</param>
         void Update<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class;
 
-        (IQueryable<TAggregateRoot>, long) PageFind<TAggregateRoot>(int pageIndex,
+        (IQueryable<TAggregateRoot> data, long total) PageFind<TAggregateRoot>(int pageIndex,
                                                                     int pageSize,
                                                                     Expression<Func<TAggregateRoot, bool>> specification,
                                                                     params OrderExpression[] orderExpressions)
             where TAggregateRoot : class;
 
 
-        Task<(IQueryable<TAggregateRoot>, long)> PageFindAsync<TAggregateRoot>(int pageIndex,
+        Task<(IQueryable<TAggregateRoot> data, long total)> PageFindAsync<TAggregateRoot>(int pageIndex,
                                                                                int pageSize,
                                                                                Expression<Func<TAggregateRoot, bool>> specification,
                                                                                params OrderExpression[] orderExpressions)
             where TAggregateRoot : class;
 
-        (IQueryable<TAggregateRoot>, long) PageFind<TAggregateRoot>(int pageIndex,
+        (IQueryable<TAggregateRoot> data, long total) PageFind<TAggregateRoot>(int pageIndex,
                                                                     int pageSize,
                                                                     ISpecification<TAggregateRoot> specification,
                                                                     params OrderExpression[] orderExpressions) where TAggregateRoot : class;
 
-        Task<(IQueryable<TAggregateRoot>, long)> PageFindAsync<TAggregateRoot>(int pageIndex,
+        Task<(IQueryable<TAggregateRoot> data, long total)> PageFindAsync<TAggregateRoot>(int pageIndex,
                                                                                int pageSize,
                                                                                ISpecification<TAggregateRoot> specification,
                                                                                params OrderExpression[] orderExpressions)
