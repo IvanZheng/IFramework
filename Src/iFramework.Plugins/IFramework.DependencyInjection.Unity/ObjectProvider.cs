bde77839e55d23386a2bba775aabc19c69ff1bcd
Ivan Zheng <yvonne_7_7@hotmail.com>
2019-10-16 10:30:39 +0800

    Add ContextStore for ObjectProvider

--- Src/iFramework.Plugins/IFramework.DependencyInjection.Unity/ObjectProvider.cs
+++ Src/iFramework.Plugins/IFramework.DependencyInjection.Unity/ObjectProvider.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using IFramework.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Unity;
-using Unity.Microsoft.DependencyInjection;
 using Unity.Resolution;
 
 namespace IFramework.DependencyInjection.Unity
 {
-    public class ObjectProvider : IObjectProvider
+    public class ObjectProvider : ObjectProviderBase
     {
-        public IUnityContainer UnityContainer { get; private set; }
         public ObjectProvider(ObjectProvider parent = null)
         {
             Parent = parent;
@@ -22,35 +20,36 @@ namespace IFramework.DependencyInjection.Unity
             SetComponentContext(container);
         }
 
-        public object GetService(Type serviceType)
+        public IUnityContainer UnityContainer { get; private set; }
+
+        public override IObjectProvider Parent { get; }
+
+        public override object GetService(Type serviceType)
         {
             return UnityContainer.Resolve(serviceType);
         }
 
         /// <summary>
-        /// 
         /// </summary>
         /// <param name="serviceType"></param>
         /// <returns></returns>
         /// <exception cref="T:System.InvalidOperationException">There is no service of type <typeparamref name="T" />.</exception>
-        public object GetRequiredService(Type serviceType)
+        public override object GetRequiredService(Type serviceType)
         {
             return UnityContainer.Resolve(serviceType) ?? throw new InvalidOperationException($"There is no service of type {serviceType.Name}");
         }
 
-        public void Dispose()
+        public override void Dispose()
         {
             UnityContainer.Dispose();
         }
 
-        public IObjectProvider Parent { get; }
-
-        public IObjectProvider CreateScope()
+        public override IObjectProvider CreateScope()
         {
             return CreateScope(new ServiceCollection());
         }
 
-        public IObjectProvider CreateScope(IServiceCollection serviceCollection)
+        public override IObjectProvider CreateScope(IServiceCollection serviceCollection)
         {
             var objectProvider = new ObjectProvider(this);
             var childScope = UnityContainer.CreateChildContainer();
@@ -62,7 +61,7 @@ namespace IFramework.DependencyInjection.Unity
             return objectProvider;
         }
 
-        public IObjectProvider CreateScope(Action<IObjectProviderBuilder> buildAction)
+        public override IObjectProvider CreateScope(Action<IObjectProviderBuilder> buildAction)
         {
             if (buildAction == null)
             {
@@ -79,32 +78,32 @@ namespace IFramework.DependencyInjection.Unity
             return objectProvider;
         }
 
-        public object GetService(Type t, string name, params Parameter[] parameters)
+        public override object GetService(Type t, string name, params Parameter[] parameters)
         {
             return UnityContainer.Resolve(t, name, GetResolverOverrides(parameters));
         }
 
-        public object GetService(Type t, params Parameter[] parameters)
+        public override object GetService(Type t, params Parameter[] parameters)
         {
             return UnityContainer.Resolve(t, GetResolverOverrides(parameters));
         }
 
-        public T GetService<T>(params Parameter[] parameters) where T : class
+        public override T GetService<T>(params Parameter[] parameters)
         {
             return UnityContainer.Resolve<T>(GetResolverOverrides(parameters));
         }
 
-        public T GetService<T>(string name, params Parameter[] parameters) where T : class
+        public override T GetService<T>(string name, params Parameter[] parameters)
         {
             return UnityContainer.Resolve<T>(name, GetResolverOverrides(parameters));
         }
 
-        public IEnumerable<object> GetAllServices(Type type, params Parameter[] parameters)
+        public override IEnumerable<object> GetAllServices(Type type, params Parameter[] parameters)
         {
             return UnityContainer.ResolveAll(type, GetResolverOverrides(parameters));
         }
 
-        public IEnumerable<T> GetAllServices<T>(params Parameter[] parameters) where T : class
+        public override IEnumerable<T> GetAllServices<T>(params Parameter[] parameters)
         {
             return UnityContainer.ResolveAll<T>(GetResolverOverrides(parameters));
         }
--- Src/iFramework.Plugins/IFramework.Microsoft.DependencyInjection/ObjectProvider.cs
+++ Src/iFramework.Plugins/IFramework.Microsoft.DependencyInjection/ObjectProvider.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace IFramework.DependencyInjection.Microsoft
 {
-    public class ObjectProvider : IObjectProvider
+    public class ObjectProvider : ObjectProviderBase
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly IServiceScope _serviceScope;
@@ -33,39 +33,39 @@ namespace IFramework.DependencyInjection.Microsoft
             Parent = parent;
         }
 
-        public void Dispose()
+        public override  void Dispose()
         {
             _serviceScope?.Dispose();
         }
 
-        public IObjectProvider Parent { get; }
+        public override  IObjectProvider Parent { get; }
 
-        public IObjectProvider CreateScope()
+        public override  IObjectProvider CreateScope()
         {
             return new ObjectProvider(_serviceProvider.CreateScope(), this);
         }
 
-        public IObjectProvider CreateScope(IServiceCollection serviceCollection)
+        public override  IObjectProvider CreateScope(IServiceCollection serviceCollection)
         {
             throw new NotImplementedException();
         }
 
-        public IObjectProvider CreateScope(Action<IObjectProviderBuilder> buildAction)
+        public override  IObjectProvider CreateScope(Action<IObjectProviderBuilder> buildAction)
         {
             throw new NotImplementedException();
         }
 
-        public object GetService(Type serviceType)
+        public override  object GetService(Type serviceType)
         {
             return _serviceProvider.GetService(serviceType);
         }
 
-        public object GetService(Type t, string name, params Parameter[] parameters)
+        public override  object GetService(Type t, string name, params Parameter[] parameters)
         {
             throw new NotImplementedException();
         }
 
-        public object GetService(Type t, params Parameter[] parameters)
+        public override  object GetService(Type t, params Parameter[] parameters)
         {
             if (parameters.Length > 0)
             {
@@ -74,7 +74,7 @@ namespace IFramework.DependencyInjection.Microsoft
             return GetService(t);
         }
 
-        public T GetService<T>(params Parameter[] overrides) where T : class
+        public override  T GetService<T>(params Parameter[] overrides)
         {
             if (overrides.Length > 0)
             {
@@ -83,12 +83,12 @@ namespace IFramework.DependencyInjection.Microsoft
             return (T) GetService(typeof(T));
         }
 
-        public T GetService<T>(string name, params Parameter[] overrides) where T : class
+        public override  T GetService<T>(string name, params Parameter[] overrides)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<object> GetAllServices(Type type, params Parameter[] parameters)
+        public override IEnumerable<object> GetAllServices(Type type, params Parameter[] parameters)
         {
             if (parameters.Length > 0)
             {
@@ -97,16 +97,16 @@ namespace IFramework.DependencyInjection.Microsoft
             return _serviceProvider.GetServices(type);
         }
 
-        public IEnumerable<T> GetAllServices<T>(params Parameter[] parameters) where T : class
+        public override IEnumerable<T> GetAllServices<T>(params Parameter[] parameters)
         {
             if (parameters.Length > 0)
             {
                 throw new NotImplementedException();
             }
-            return GetAllServices(typeof(T)).Cast<T>();
+            return GetAllServices(typeof(T), default(Parameter[])).Cast<T>();
         }
 
-        public object GetRequiredService(Type serviceType)
+        public  override object GetRequiredService(Type serviceType)
         {
             return _serviceProvider.GetRequiredService(serviceType);
         }
