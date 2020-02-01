fcba1ba0530603a71dea657e687ebf2800766bc6
Ivan <yvonne_7_7@hotmail.com>
2020-02-01 17:05:40 +0800

    Implement CreateAccount handler

--- Src/Sample/Sample.Domain/Model/Bank/Accounts/BankAccount.cs
+++ Src/Sample/Sample.Domain/Model/Bank/Accounts/BankAccount.cs
@@ -1,60 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using IFramework.Event;
-using IFramework.Infrastructure.EventSourcing.Domain;
+﻿using IFramework.Infrastructure.EventSourcing.Domain;
 using Sample.Command.Banks;
+using Sample.DomainEvents.Banks;
 
 namespace Sample.Domain.Model.BankAccounts
 {
-    public class BankAccount: EventSourcingAggregateRoot
+    public class BankAccount : EventSourcingAggregateRoot
     {
-      
         public string Name { get; private set; }
         public string CardId { get; private set; }
+
         /// <summary>
-        /// 可用余额
+        ///     可用余额
         /// </summary>
-        public decimal AvailableAmount { get; private set; }
+        public decimal AvailableFund { get; private set; }
+
         /// <summary>
-        /// 总金额
+        ///     总金额
         /// </summary>
-        public decimal TotalAmount { get; private set; }
+        public decimal TotalFund { get; private set; }
+
         /// <summary>
-        /// 账户状态
+        ///     账户状态
         /// </summary>
         public AccountStatus Status { get; private set; }
 
-        public BankAccount(){}
-
-        public BankAccount(string id, string name, string cardId, decimal amount)
+        private void Handle(AccountCreated accountCreated)
         {
-            Id = id;
-            Name = name;
-            CardId = cardId;
-            AvailableAmount = amount;
-            TotalAmount = amount;
+            Id = accountCreated.Id;
+            Name = accountCreated.Name;
+            CardId = accountCreated.CardId;
+            AvailableFund = accountCreated.Amount;
+            TotalFund = accountCreated.Amount;
             Status = AccountStatus.Normal;
         }
 
-        public void PrepareCredit(decimal amount, string transactionId)
+        public BankAccount(string id, string name, string cardId, decimal amount)
         {
-
+            Initialize(id, name, cardId, amount);
         }
 
-        public void CommitCredit(decimal amount, string transactionId)
+        protected void Initialize(string id, string name, string cardId, decimal amount)
         {
-
+            OnEvent(new AccountCreated(id, amount, name, cardId));
         }
 
-        public void PrepareDebit(decimal amount, string transactionId)
-        {
+        public void PrepareCredit(decimal amount, string transactionId) { }
 
-        }
+        public void CommitCredit(decimal amount, string transactionId) { }
 
-        public void RevertDebitPreparation(decimal amount, string transactionId)
-        {
+        public void PrepareDebit(decimal amount, string transactionId) { }
 
-        }
+        public void RevertDebitPreparation(decimal amount, string transactionId) { }
     }
 }
