83251cb2ef1d8416be7b2a303c4bd70453c6b874
IvanZheng <yvonne_7_7@hotmail.com>
2024-01-22 09:13:24 +0800

    integrated with efcore8.0

--- Src/iFramework.Plugins/IFramework.EntityFrameworkCore/ExtensionEntityMaterializerSource.cs
+++ Src/iFramework.Plugins/IFramework.EntityFrameworkCore/ExtensionEntityMaterializerSource.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using IFramework.Domain;
+using IFramework.Infrastructure;
 using IFramework.Repositories;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Query;
@@ -15,28 +16,43 @@ namespace IFramework.EntityFrameworkCore
     public class ExtensionEntityMaterializerSource : EntityMaterializerSource
 #pragma warning restore EF1001 // Internal EF Core API usage.
     {
-        public override Expression CreateMaterializeExpression(IEntityType entityType,
-                                                               string entityInstanceName,
-                                                               Expression materializationExpression)
+        public override Func<MaterializationContext, object> GetMaterializer(IEntityType entityType)
         {
-#pragma warning disable EF1001 // Internal EF Core API usage.
-            var expression = base.CreateMaterializeExpression(entityType, entityInstanceName, materializationExpression);
-#pragma warning restore EF1001 // Internal EF Core API usage.
-            if (typeof(Entity).IsAssignableFrom(entityType.ClrType) && expression is BlockExpression blockExpression)
+            var func = base.GetMaterializer(entityType);
+
+            return (context) =>
             {
-                var property = Expression.Property(blockExpression.Variables[0],
-                                                   typeof(Entity).GetProperty("DbContext",
-                                                                              BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException()); //赋值表达式
-                var assign = Expression.Assign(property, 
-                                               Expression.TypeAs(Expression.Property(materializationExpression, 
-                                                                                     typeof(MaterializationContext).GetProperty("Context") ?? throw new InvalidOperationException()),
-                                                                 typeof(IDbContext))); //把基类的实例化表达式变成列表方便插入
-                var list = blockExpression.Expressions.ToList(); //因为最后一个表达式是返回实体实例<br>                //所以我们的逻辑代码要放在最后一条语句之前
-                list.Insert(list.Count - 1, assign); //重新生成表达式
-                expression = Expression.Block(blockExpression.Variables, list);
-            }
-            return expression;
+                var obj = func(context);
+                if (obj is Entity entity)
+                {
+                    entity.SetValueByKey("Context", context.Context);
+                }
+                return obj;
+            };
         }
+
+//        public override Expression CreateMaterializeExpression(IEntityType entityType,
+//                                                               string entityInstanceName,
+//                                                               Expression materializationExpression)
+//        {
+//#pragma warning disable EF1001 // Internal EF Core API usage.
+//            var expression = base.CreateMaterializeExpression(entityType, entityInstanceName, materializationExpression);
+//#pragma warning restore EF1001 // Internal EF Core API usage.
+//            if (typeof(Entity).IsAssignableFrom(entityType.ClrType) && expression is BlockExpression blockExpression)
+//            {
+//                var property = Expression.Property(blockExpression.Variables[0],
+//                                                   typeof(Entity).GetProperty("DbContext",
+//                                                                              BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException()); //赋值表达式
+//                var assign = Expression.Assign(property, 
+//                                               Expression.TypeAs(Expression.Property(materializationExpression, 
+//                                                                                     typeof(MaterializationContext).GetProperty("Context") ?? throw new InvalidOperationException()),
+//                                                                 typeof(IDbContext))); //把基类的实例化表达式变成列表方便插入
+//                var list = blockExpression.Expressions.ToList(); //因为最后一个表达式是返回实体实例<br>                //所以我们的逻辑代码要放在最后一条语句之前
+//                list.Insert(list.Count - 1, assign); //重新生成表达式
+//                expression = Expression.Block(blockExpression.Variables, list);
+//            }
+//            return expression;
+//        }
 
 #pragma warning disable EF1001 // Internal EF Core API usage.
         public ExtensionEntityMaterializerSource(EntityMaterializerSourceDependencies dependencies) : base(dependencies)
