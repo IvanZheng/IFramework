22165d94eaaa4bc4f8b01c10c098f76e018506b9
IvanZheng <yvonne_7_7@hotmail.com>
2024-01-22 13:30:10 +0800

    update to efcore8.0

--- Src/iFramework.Plugins/IFramework.EntityFrameworkCore/ExtensionEntityMaterializerSource.cs
+++ Src/iFramework.Plugins/IFramework.EntityFrameworkCore/ExtensionEntityMaterializerSource.cs
@@ -16,6 +16,7 @@ namespace IFramework.EntityFrameworkCore
     public class ExtensionEntityMaterializerSource : EntityMaterializerSource
 #pragma warning restore EF1001 // Internal EF Core API usage.
     {
+#if NET8_0
         public override Func<MaterializationContext, object> GetMaterializer(IEntityType entityType)
         {
             var func = base.GetMaterializer(entityType);
@@ -30,29 +31,47 @@ namespace IFramework.EntityFrameworkCore
                 return obj;
             };
         }
+       
+        public override Func<MaterializationContext, object> GetEmptyMaterializer(IEntityType entityType)
+        {
+            var func = base.GetEmptyMaterializer(entityType);
 
-//        public override Expression CreateMaterializeExpression(IEntityType entityType,
-//                                                               string entityInstanceName,
-//                                                               Expression materializationExpression)
-//        {
-//#pragma warning disable EF1001 // Internal EF Core API usage.
-//            var expression = base.CreateMaterializeExpression(entityType, entityInstanceName, materializationExpression);
-//#pragma warning restore EF1001 // Internal EF Core API usage.
-//            if (typeof(Entity).IsAssignableFrom(entityType.ClrType) && expression is BlockExpression blockExpression)
-//            {
-//                var property = Expression.Property(blockExpression.Variables[0],
-//                                                   typeof(Entity).GetProperty("DbContext",
-//                                                                              BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException()); //赋值表达式
-//                var assign = Expression.Assign(property, 
-//                                               Expression.TypeAs(Expression.Property(materializationExpression, 
-//                                                                                     typeof(MaterializationContext).GetProperty("Context") ?? throw new InvalidOperationException()),
-//                                                                 typeof(IDbContext))); //把基类的实例化表达式变成列表方便插入
-//                var list = blockExpression.Expressions.ToList(); //因为最后一个表达式是返回实体实例<br>                //所以我们的逻辑代码要放在最后一条语句之前
-//                list.Insert(list.Count - 1, assign); //重新生成表达式
-//                expression = Expression.Block(blockExpression.Variables, list);
-//            }
-//            return expression;
-//        }
+            return (context) =>
+            {
+                var obj = func(context);
+                if (obj is Entity entity)
+                {
+                    entity.SetValueByKey("Context", context.Context);
+                }
+                return obj;
+            };
+        }
+#endif
+        #if NET6_0_OR_GREATER && !NET8_0
+        public override Expression CreateMaterializeExpression(IEntityType entityType,
+                                                               string entityInstanceName,
+                                                               Expression materializationExpression)
+        {
+#pragma warning disable EF1001 // Internal EF Core API usage.
+            var expression = base.CreateMaterializeExpression(entityType, entityInstanceName, materializationExpression);
+#pragma warning restore EF1001 // Internal EF Core API usage.
+            if (typeof(Entity).IsAssignableFrom(entityType.ClrType) && expression is BlockExpression blockExpression)
+            {
+                var property = Expression.Property(blockExpression.Variables[0],
+                                                   typeof(Entity).GetProperty("DbContext",
+                                                                              BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException()); //赋值表达式
+                var assign = Expression.Assign(property,
+                                               Expression.TypeAs(Expression.Property(materializationExpression,
+                                                                                     typeof(MaterializationContext).GetProperty("Context") ?? throw new InvalidOperationException()),
+                                                                 typeof(IDbContext))); //把基类的实例化表达式变成列表方便插入
+                var list = blockExpression.Expressions.ToList(); //因为最后一个表达式是返回实体实例<br>                //所以我们的逻辑代码要放在最后一条语句之前
+                list.Insert(list.Count - 1, assign); //重新生成表达式
+                expression = Expression.Block(blockExpression.Variables, list);
+            }
+            return expression;
+        }
+#endif
+
 
 #pragma warning disable EF1001 // Internal EF Core API usage.
         public ExtensionEntityMaterializerSource(EntityMaterializerSourceDependencies dependencies) : base(dependencies)
--- Src/iFramework/Repositories/IDomainRepository.cs
+++ Src/iFramework/Repositories/IDomainRepository.cs
@@ -17,42 +17,35 @@ namespace IFramework.Repositories
         ///     Adds an entity to the repository.
         /// </summary>
         /// <param name="entities"></param>
-        void Add<TAggregateRoot>(IEnumerable<TAggregateRoot> entities) where TAggregateRoot : class;
+        void Add<TAggregateRoot>(IEnumerable<TAggregateRoot> entities);
 
-        void Add<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class;
+        void Add<TAggregateRoot>(TAggregateRoot entity) ;
 
         /// <summary>
         ///     Gets the entity instance from repository by a given key.
         /// </summary>
         /// <param name="keyValues"></param>
         /// <returns>The instance of the entity.</returns>
-        TAggregateRoot GetByKey<TAggregateRoot>(params object[] keyValues) where TAggregateRoot : class;
+        TAggregateRoot GetByKey<TAggregateRoot>(params object[] keyValues);
 
-        Task<TAggregateRoot> GetByKeyAsync<TAggregateRoot>(params object[] keyValues)
-            where TAggregateRoot : class;
-
-
-        long Count<TAggregateRoot>()
-            where TAggregateRoot : class;
-
-        Task<long> CountAsync<TAggregateRoot>()
-            where TAggregateRoot : class;
-
-        long Count<TAggregateRoot>(ISpecification<TAggregateRoot> specification)
-            where TAggregateRoot : class;
-
-        Task<long> CountAsync<TAggregateRoot>(ISpecification<TAggregateRoot> specification)
-            where TAggregateRoot : class;
-
-        long Count<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification)
-            where TAggregateRoot : class;
-
-        Task<long> CountAsync<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification)
-            where TAggregateRoot : class;
+        Task<TAggregateRoot> GetByKeyAsync<TAggregateRoot>(params object[] keyValues);
 
 
-        void Reload<TEntity>(TEntity entity) where TEntity : Entity;
-        Task ReloadAsync<TEntity>(TEntity entity) where TEntity : Entity;
+        long Count<TAggregateRoot>();
+
+        Task<long> CountAsync<TAggregateRoot>();
+
+        long Count<TAggregateRoot>(ISpecification<TAggregateRoot> specification);
+
+        Task<long> CountAsync<TAggregateRoot>(ISpecification<TAggregateRoot> specification);
+
+        long Count<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification);
+
+        Task<long> CountAsync<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification);
+
+
+        void Reload<TEntity>(TEntity entity) where TEntity : IEntity;
+        Task ReloadAsync<TEntity>(TEntity entity) where TEntity : IEntity;
 
         /// <summary>
         ///     Finds all the aggregate roots from repository, sorting by using the provided sort predicate
@@ -63,8 +56,7 @@ namespace IFramework.Repositories
         ///     All the aggregate roots got from the repository, with the aggregate roots being sorted by
         ///     using the provided sort predicate and the sort order.
         /// </returns>
-        IQueryable<TAggregateRoot> FindAll<TAggregateRoot>(params OrderExpression[] orderExpressions)
-            where TAggregateRoot : class;
+        IQueryable<TAggregateRoot> FindAll<TAggregateRoot>(params OrderExpression[] orderExpressions);
 
         /// <summary>
         ///     <summary>
@@ -79,81 +71,70 @@ namespace IFramework.Repositories
         ///     </returns>
         /// </summary>
         IQueryable<TAggregateRoot> FindAll<TAggregateRoot>(ISpecification<TAggregateRoot> specification,
-                                                           params OrderExpression[] orderExpressions) where TAggregateRoot : class;
+                                                           params OrderExpression[] orderExpressions);
 
         IQueryable<TAggregateRoot> FindAll<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification,
-                                                           params OrderExpression[] orderExpressions) where TAggregateRoot : class;
+                                                           params OrderExpression[] orderExpressions);
 
         /// <summary>
         ///     Finds a single aggregate root that matches the given specification.
         /// </summary>
         /// <param name="specification">The specification with which the aggregate root should match.</param>
         /// <returns>The instance of the aggregate root.</returns>
-        TAggregateRoot Find<TAggregateRoot>(ISpecification<TAggregateRoot> specification)
-            where TAggregateRoot : class;
+        TAggregateRoot Find<TAggregateRoot>(ISpecification<TAggregateRoot> specification);
 
-        Task<TAggregateRoot> FindAsync<TAggregateRoot>(ISpecification<TAggregateRoot> specification)
-            where TAggregateRoot : class;
+        Task<TAggregateRoot> FindAsync<TAggregateRoot>(ISpecification<TAggregateRoot> specification);
 
-        TAggregateRoot Find<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification)
-            where TAggregateRoot : class;
+        TAggregateRoot Find<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification);
 
-        Task<TAggregateRoot> FindAsync<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification)
-            where TAggregateRoot : class;
+        Task<TAggregateRoot> FindAsync<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification);
 
         /// <summary>
         ///     Checkes whether the aggregate root which matches the given specification exists.
         /// </summary>
         /// <param name="specification">The specification with which the aggregate root should match.</param>
         /// <returns>True if the aggregate root exists, otherwise false.</returns>
-        bool Exists<TAggregateRoot>(ISpecification<TAggregateRoot> specification)
-            where TAggregateRoot : class;
+        bool Exists<TAggregateRoot>(ISpecification<TAggregateRoot> specification);
 
-        Task<bool> ExistsAsync<TAggregateRoot>(ISpecification<TAggregateRoot> specification)
-            where TAggregateRoot : class;
+        Task<bool> ExistsAsync<TAggregateRoot>(ISpecification<TAggregateRoot> specification);
 
-        bool Exists<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification)
-            where TAggregateRoot : class;
+        bool Exists<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification);
 
-        Task<bool> ExistsAsync<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification)
-            where TAggregateRoot : class;
+        Task<bool> ExistsAsync<TAggregateRoot>(Expression<Func<TAggregateRoot, bool>> specification);
 
         /// <summary>
         ///     Removes the entity from the repository.
         /// </summary>
         /// <param name="entity">The entity to be removed.</param>
-        void Remove<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class;
+        void Remove<TAggregateRoot>(TAggregateRoot entity);
 
-        void Remove<TAggregateRoot>(IEnumerable<TAggregateRoot> entities) where TAggregateRoot : class;
+        void Remove<TAggregateRoot>(IEnumerable<TAggregateRoot> entities);
 
         /// <summary>
         ///     Updates the entity in the repository.
         /// </summary>
         /// <param name="entity">The entity to be updated.</param>
-        void Update<TAggregateRoot>(TAggregateRoot entity) where TAggregateRoot : class;
+        void Update<TAggregateRoot>(TAggregateRoot entity);
 
         (IQueryable<TAggregateRoot> DataQueryable, long Total) PageFind<TAggregateRoot>(int pageIndex,
                                                                     int pageSize,
                                                                     Expression<Func<TAggregateRoot, bool>> specification,
-                                                                    params OrderExpression[] orderExpressions)
-            where TAggregateRoot : class;
+                                                                    params OrderExpression[] orderExpressions);
 
 
         Task<(IQueryable<TAggregateRoot> DataQueryable, long Total)> PageFindAsync<TAggregateRoot>(int pageIndex,
                                                                                int pageSize,
                                                                                Expression<Func<TAggregateRoot, bool>> specification,
-                                                                               params OrderExpression[] orderExpressions)
-            where TAggregateRoot : class;
+                                                                               params OrderExpression[] orderExpressions);
 
         (IQueryable<TAggregateRoot> DataQueryable, long Total) PageFind<TAggregateRoot>(int pageIndex,
                                                                     int pageSize,
                                                                     ISpecification<TAggregateRoot> specification,
-                                                                    params OrderExpression[] orderExpressions) where TAggregateRoot : class;
+                                                                    params OrderExpression[] orderExpressions);
 
         Task<(IQueryable<TAggregateRoot> DataQueryable, long Total)> PageFindAsync<TAggregateRoot>(int pageIndex,
                                                                                int pageSize,
                                                                                ISpecification<TAggregateRoot> specification,
-                                                                               params OrderExpression[] orderExpressions)
-            where TAggregateRoot : class;
+                                                                               params OrderExpression[] orderExpressions);
     }
 }
