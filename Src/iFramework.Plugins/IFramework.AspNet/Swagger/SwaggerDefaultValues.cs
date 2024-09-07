d510b46e96c0381fe1fabc73fb261ed5b34b37b6
Ivan Zheng <yvonne_7_7@hotmail.com>
2024-09-07 20:34:13 +0800

    add swagger utility and snowIdWorker

---
+++ Src/iFramework.Plugins/IFramework.AspNet/Swagger/SwaggerDefaultValues.cs
@@ -0,0 +1,70 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace IFramework.AspNet.Swagger
+{
+    public static class AttributeExtension
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+            where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .FirstOrDefault()?
+                            .GetCustomAttribute<TAttribute>();
+        }
+
+        public static DescriptionAttribute GetDescriptionAttribute(this Enum enumValue)
+        {
+            return enumValue.GetAttribute<DescriptionAttribute>();
+        }
+    }
+    /// <summary>
+    /// Represents the Swagger/Swashbuckle operation filter used to document the implicit API version parameter.
+    /// </summary>
+    /// <remarks>This <see cref="IOperationFilter"/> is only required due to bugs in the <see cref="SwaggerGenerator"/>.
+    /// Once they are fixed and published, this class can be removed.</remarks>
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        /// <summary>
+        /// Applies the filter to the specified operation using the given context.
+        /// </summary>
+        /// <param name="operation">The operation to apply the filter to.</param>
+        /// <param name="context">The current operation filter context.</param>
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+
+            //operation.Deprecated |= apiDescription.IsDeprecated();
+
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/412
+            // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/pull/413
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                if (parameter.Schema.Default == null && description.DefaultValue != null)
+                {
+                    parameter.Schema.Default = new OpenApiString(description.DefaultValue.ToString());
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
+        }
+    }
+}
---
+++ Src/iFramework/Infrastructure/SnowIdWorker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using IFramework.Config;
+using Microsoft.Extensions.Configuration;
+using Snowflake.Core;
+
+namespace IFramework.Infrastructure
+{
+    public class SnowIdWorker
+    {
+        private static Lazy<IdWorker> Instance = new Lazy<IdWorker>(() =>
+        {
+            var workerId = Configuration.Instance.GetValue<long?>($"{nameof(SnowIdWorker)}:Id") ?? 0;
+            var dataCenterId = Configuration.Instance.GetValue<long?>($"{nameof(SnowIdWorker)}:DataCenterId") ?? 0;
+            var (workerIdFromIp, dataCenterIdFromIp) = GenerateIdsFromIp(GetLocalIpAddress());
+            if (workerId == 0)
+            {
+                workerId = workerIdFromIp;
+            }
+
+            if (dataCenterId == 0)
+            {
+                dataCenterId = dataCenterIdFromIp;
+            }
+
+            return new IdWorker(workerId, dataCenterId);
+        });
+
+        public static string GenerateId()
+        {
+            return Instance.Value.NextId().ToString();
+        }
+
+        private static string GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            var ip = host.AddressList.FirstOrDefault(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            return ip?.ToString() ?? throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private static (long workerId, long datacenterId) GenerateIdsFromIp(string ipAddress)
+        {
+            var ipParts = ipAddress.Split('.').Select(int.Parse).ToArray();
+            long workerId = ipParts[2] % 32; // 使用第三段生成 workerId
+            long datacenterId = ipParts[3] % 32; // 使用第四段生成 datacenterId
+            return (workerId, datacenterId);
+        }
+    }
+}
