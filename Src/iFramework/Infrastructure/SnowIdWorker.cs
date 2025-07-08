210b042d58058113bcbfa84d2580df508a32ba7b
Ivan Zheng <yvonne_7_7@hotmail.com>
2025-07-08 15:41:32 +0800

    add SnowIdWorker.ExtractTimestamp

--- Src/iFramework/Infrastructure/SnowIdWorker.cs
+++ Src/iFramework/Infrastructure/SnowIdWorker.cs
@@ -11,6 +11,17 @@ namespace IFramework.Infrastructure
 {
     public class SnowIdWorker
     {
+        public static readonly DateTime Epoch = ConvertTwepochToDateTime(IdWorker.Twepoch);
+
+        public static DateTime ConvertTwepochToDateTime(long twepoch)
+        {
+            // Unix epoch 起点
+            DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            // 加上 Twepoch 毫秒数
+            return unixEpoch.AddMilliseconds(twepoch);
+        }
+
+
         public static Lazy<IdWorker> Instance = new Lazy<IdWorker>(() =>
         {
             var workerId = Configuration.Instance.GetValue<long?>($"{nameof(SnowIdWorker)}:Id") ?? 0;
@@ -37,6 +48,23 @@ namespace IFramework.Infrastructure
         public static long GenerateLongId()
         {
             return Instance.Value.NextId();
+        }
+
+        public static DateTime ExtractTimestamp(string snowflakeId)
+        {
+            if (long.TryParse(snowflakeId, out var timestamp))
+            {
+                return ExtractTimestamp(timestamp);
+            }
+            return DateTime.MinValue;
+        }
+
+        public static DateTime ExtractTimestamp(long snowflakeId)
+        {
+            // 右移 22 位，得到 41 位的时间戳部分
+            long timestampPart = (snowflakeId >> 22);
+            // 加上 epoch 得到实际时间
+            return Epoch.AddMilliseconds(timestampPart).ToLocalTime();
         }
 
         private static string GetLocalIpAddress()
